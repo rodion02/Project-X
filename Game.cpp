ef65425a3691d5faafd75335c3830d97dbbb11db
Acssar <romaderunec@gmail.com>
2022-03-18 11:43:44 +0000

    add library

---
+++ Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by roman on 2/23/22.
+//
+
+#include "Game.h"
---
+++ Game.h
@@ -0,0 +1,14 @@
+//
+// Created by roman on 2/23/22.
+//
+
+#ifndef YPPRPO_GAME_H
+#define YPPRPO_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //YPPRPO_GAME_H
