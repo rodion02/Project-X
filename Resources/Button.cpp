70cd0aa41dc59b782424d33de80ca116cba1a1a5
Acssar <romaderunec@gmail.com>
2022-05-20 14:08:18 +0700

    add fonts

---
+++ Resources/Button.cpp
@@ -0,0 +1,5 @@
+//
+// Created by roman on 5/20/22.
+//
+
+#include "Button.h"
---
+++ Resources/Button.h
@@ -0,0 +1,14 @@
+//
+// Created by roman on 5/20/22.
+//
+
+#ifndef YPPRPO_BUTTON_H
+#define YPPRPO_BUTTON_H
+
+
+class Button {
+
+};
+
+
+#endif //YPPRPO_BUTTON_H
