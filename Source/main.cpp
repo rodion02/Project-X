a49cf9f15ea38464b7c21bbcdea77ad44c963538
Acssar <romaderunec@gmail.com>
2022-06-23 03:28:22 +0700

    CMake rewert

--- Source/main.cpp
+++ Source/main.cpp
@@ -16,9 +16,9 @@
 int main(const int argc, const char* argv[])
 {
     Catch::Session().run(argc, argv);
-//Uncomment to run GAME
-//    Game game;
-//    game.run();
+
+    Game game;
+    game.run();
 
     return 0;
 }
